366a7e5f1f52b499e0dbb28763fa268ac2454421
Javier Valencia <javiervalencia80@gmail.com>
2011-05-23 09:48:48 +0100

    added some speed improvements

--- atmega-scheduler.c
+++ atmega-scheduler.c
@@ -35,7 +35,7 @@ volatile struct _sched_task_t _sched_tasks[SCHEDULER_MAX_TASKS];
 void scheduler_init(uint8_t hour, uint8_t minutes) {
 	int16_t index;
 	
-	TIMSK1 &= ~(1<<OCIE1A);
+	TIMSK1 &= ~(_BV(OCIE1A));
 	
 	for(index=0; index<SCHEDULER_MAX_TASKS; index++)
 		_sched_tasks[index].task = NULL;
@@ -46,17 +46,17 @@ void scheduler_init(uint8_t hour, uint8_t minutes) {
 	
 	OCR1A = SCHEDULER_TICKS;
 	TCCR1A = 0;
-	TCCR1B = SCHEDULER_PRESCALER | (1<<WGM12);
+	TCCR1B = SCHEDULER_PRESCALER | _BV(WGM12);
 	TCNT1 = 0;
-	TIMSK1 |= (1<<OCIE1A);
+	TIMSK1 |= _BV(OCIE1A);
 }
 
 void scheduler_set(uint8_t hour, uint8_t minutes) {
-	TIMSK1 &= ~(1<<OCIE1A);
+	TIMSK1 &= ~(_BV(OCIE1A));
 	_sched_hours = hour;
 	_sched_minutes = minutes;
 	_sched_seconds = 0;
-	TIMSK1 |= (1<<OCIE1A);
+	TIMSK1 |= _BV(OCIE1A);
 }
 
 int16_t task_add(uint8_t hour, uint8_t minutes, void (*f)()) {
@@ -66,11 +66,11 @@ int16_t task_add(uint8_t hour, uint8_t minutes, void (*f)()) {
 	for(index=0; index<SCHEDULER_MAX_TASKS; index++) {
 		task = &_sched_tasks[index];
 		if (task->task == NULL) {
-			TIMSK1 &= ~(1<<OCIE1A);
+			TIMSK1 &= ~(_BV(OCIE1A));
 			task->hour = hour;
 			task->minutes = minutes;
 			task->task = f;
-			TIMSK1 |= (1<<OCIE1A);
+			TIMSK1 |= _BV(OCIE1A);
 			return index;
 		}
 	}
@@ -82,10 +82,10 @@ void task_del(int16_t index) {
 }
 
 void task_set(int16_t index, uint8_t hour, uint8_t minutes) {
-	TIMSK1 &= ~(1<<OCIE1A);
+	TIMSK1 &= ~(_BV(OCIE1A));
 	_sched_tasks[index].hour = hour;
 	_sched_tasks[index].minutes = minutes;
-	TIMSK1 |= (1<<OCIE1A);
+	TIMSK1 |= _BV(OCIE1A);
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -93,8 +93,8 @@ ISR(TIMER1_COMPA_vect) {
 	volatile struct _sched_task_t *task;
 
 	_sched_seconds += 1;
-	if (_sched_seconds >= 60) {
-		_sched_seconds = _sched_seconds - 60;
+	if (_sched_seconds == 60) {
+		_sched_seconds = 0;
 		_sched_minutes++;
 		if (_sched_minutes == 60) {
 			_sched_minutes = 0;
